f9738f4ef70d3bfb58d9a438f8244bd2f3cf99a5
Lennie Wennerlund <lennie@lempa.se>
2021-11-27 01:22:01 +0100

    WIP

---
+++ ClientBackupHelper/Services/ClientGetInformationService.cs
@@ -0,0 +1,18 @@
+﻿// <summary>
+// ClientGetInformationService
+// </summary>
+// <copyright file="ClientGetInformationService.cs" company="LiSoLi">
+// Copyright (c) LiSoLi. All rights reserved.
+// </copyright>
+// <author>Lennie Wennerlund (lempa)</author>
+
+namespace LiBackup.ClientBackupHelper.Services
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class ClientGetInformationService
+    {
+    }
+}
